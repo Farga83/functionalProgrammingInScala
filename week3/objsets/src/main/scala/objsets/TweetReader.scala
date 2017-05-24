750646a4a2f3360f45c5a91d2e213780c6def206
Jeremy Brown <jbrown@maana.io>
2017-05-24 13:58:39 -0700

    scalafmt on save

--- week3/objsets/src/main/scala/objsets/TweetReader.scala
+++ week3/objsets/src/main/scala/objsets/TweetReader.scala
@@ -37,40 +37,62 @@ object TweetReader {
     val buf = new StringBuffer
     for (tw <- tws) {
       val json = "{ \"user\": \"" + tw.user + "\", \"text\": \"" +
-                                    tw.text.replaceAll(""""""", "\\\\\\\"") + "\", \"retweets\": " +
-                                    tw.retweets + ".0 }"
+        tw.text.replaceAll(""""""", "\\\\\\\"") + "\", \"retweets\": " +
+        tw.retweets + ".0 }"
       buf.append(json + ",\n")
     }
     buf.toString
   }
 
-  val sites = List("gizmodo", "TechCrunch", "engadget", "amazondeals", "CNET", "gadgetlab", "mashable")
+  val sites = List("gizmodo",
+                   "TechCrunch",
+                   "engadget",
+                   "amazondeals",
+                   "CNET",
+                   "gadgetlab",
+                   "mashable")
 
-  private val gizmodoTweets = TweetReader.ParseTweets.getTweetData("gizmodo", TweetData.gizmodo)
-  private val techCrunchTweets = TweetReader.ParseTweets.getTweetData("TechCrunch", TweetData.TechCrunch)
-  private val engadgetTweets = TweetReader.ParseTweets.getTweetData("engadget", TweetData.engadget)
-  private val amazondealsTweets = TweetReader.ParseTweets.getTweetData("amazondeals", TweetData.amazondeals)
-  private val cnetTweets = TweetReader.ParseTweets.getTweetData("CNET", TweetData.CNET)
-  private val gadgetlabTweets = TweetReader.ParseTweets.getTweetData("gadgetlab", TweetData.gadgetlab)
-  private val mashableTweets = TweetReader.ParseTweets.getTweetData("mashable", TweetData.mashable)
+  private val gizmodoTweets =
+    TweetReader.ParseTweets.getTweetData("gizmodo", TweetData.gizmodo)
+  private val techCrunchTweets =
+    TweetReader.ParseTweets.getTweetData("TechCrunch", TweetData.TechCrunch)
+  private val engadgetTweets =
+    TweetReader.ParseTweets.getTweetData("engadget", TweetData.engadget)
+  private val amazondealsTweets =
+    TweetReader.ParseTweets.getTweetData("amazondeals", TweetData.amazondeals)
+  private val cnetTweets =
+    TweetReader.ParseTweets.getTweetData("CNET", TweetData.CNET)
+  private val gadgetlabTweets =
+    TweetReader.ParseTweets.getTweetData("gadgetlab", TweetData.gadgetlab)
+  private val mashableTweets =
+    TweetReader.ParseTweets.getTweetData("mashable", TweetData.mashable)
 
-  private val sources = List(gizmodoTweets, techCrunchTweets, engadgetTweets, amazondealsTweets, cnetTweets, gadgetlabTweets, mashableTweets)
+  private val sources = List(gizmodoTweets,
+                             techCrunchTweets,
+                             engadgetTweets,
+                             amazondealsTweets,
+                             cnetTweets,
+                             gadgetlabTweets,
+                             mashableTweets)
 
   val tweetMap: Map[String, List[Tweet]] =
-    Map() ++ Seq((sites(0) -> gizmodoTweets),
-                 (sites(1) -> techCrunchTweets),
-                 (sites(2) -> engadgetTweets),
-                 (sites(3) -> amazondealsTweets),
-                 (sites(4) -> cnetTweets),
-                 (sites(5) -> gadgetlabTweets),
-                 (sites(6) -> mashableTweets))
+    Map() ++ Seq(
+      (sites(0) -> gizmodoTweets),
+      (sites(1) -> techCrunchTweets),
+      (sites(2) -> engadgetTweets),
+      (sites(3) -> amazondealsTweets),
+      (sites(4) -> cnetTweets),
+      (sites(5) -> gadgetlabTweets),
+      (sites(6) -> mashableTweets)
+    )
 
   val tweetSets: List[TweetSet] = sources.map(tweets => toTweetSet(tweets))
 
   private val siteTweetSetMap: Map[String, TweetSet] =
     Map() ++ (sites zip tweetSets)
 
-  private def unionOfAllTweetSets(curSets: List[TweetSet], acc: TweetSet): TweetSet =
+  private def unionOfAllTweetSets(curSets: List[TweetSet],
+                                  acc: TweetSet): TweetSet =
     if (curSets.isEmpty) acc
     else unionOfAllTweetSets(curSets.tail, acc.union(curSets.head))
 
